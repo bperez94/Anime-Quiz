0622f2021ef6524d9a8b7319e97dd388aa6e7b97
Brandon Perez <brandonp940@gmail.com>
2020-06-11 12:19:39 -0400

    tried to add timer

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // when person clicks start they are redirectd to question 1 page
-document.getElementById("startButton").addEventListener("click",startGame);
-
 function startGame() {
     window.location.href = "Q1index.html";
 
 }
 
+function myFunction() {
+    alert("correct");
+}
+// funtion, if else statement to alert user answer is right or wrong
+function page1() {
+    var sonGoku = document.getElementById("Goku");
+    
 
+    if (sonGoku.addEventListener("click", myFunction)) {
+        alert(correct);
+    } else {
+        
+    }
+}
+
+document.getElementById("startButton").addEventListener("click",startGame);
