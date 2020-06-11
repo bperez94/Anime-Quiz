c42b935e86892581adc9b71dcfc2564a8c6a3010
Brandon Perez <brandonp940@gmail.com>
2020-06-11 09:50:39 -0400

    js file only start click funtion added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// when person clicks start they are redirectd to question 1 page
+document.getElementById("startButton").addEventListener("click",startGame);
+
+function startGame() {
+    window.location.href = "Q1index.html";
+
+}
+
 
