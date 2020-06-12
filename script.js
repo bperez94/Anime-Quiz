3c7589f60c6352684b750bfeead96d337637dd08
Brandon Perez <brandonp940@gmail.com>
2020-06-11 21:51:25 -0400

    added comments for psudeo code

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+//global variables
+
 // when person clicks start they are redirectd to question 1 page
 function startGame() {
     window.location.href = "Q1index.html";
 
 }
-// timer
-var elTime = document.querySelector("timeDisplay");
+ /*timer (could not get funtion to work)
+ var elTime = document.querySelector("timeDisplay");
 
 function setTime() {
+
     secondsLeft = 60;
     var timerInterval = setInterval(function name() {
         secondsLeft--;
@@ -18,7 +21,7 @@ function setTime() {
         } 1000;
     })
 }
-
+*/
 // funtion, if else statement to alert user answer is right or wrong
 function page1() {
     var sonGoku = document.getElementById("Goku");
@@ -30,6 +33,8 @@ function page1() {
         
     }
 }
-
+// when start button is clicked you are taken to the first question
+//needs to be specific to start page as to not effect other pages
 document.getElementById("startButton").addEventListener("click",startGame);
     event.preventDefault();
+// use format from above so when next button is clicked it takes you to next question
