dcf143cf64d1a39b5d5116556805d4263fde70d9
Brandon Perez <brandonp940@gmail.com>
2020-06-11 21:04:04 -0400

    added timer, need to fix bugs

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ function startGame() {
     window.location.href = "Q1index.html";
 
 }
+// timer
+var elTime = document.querySelector("timeDisplay");
 
-function myFunction() {
-    alert("correct");
+function setTime() {
+    secondsLeft = 60;
+    var timerInterval = setInterval(function name() {
+        secondsLeft--;
+        timerInterval.Display.textcontent = secondsLeft + "seconds remaining";
+
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+            sendMessage();
+        } 1000;
+    })
 }
+
 // funtion, if else statement to alert user answer is right or wrong
 function page1() {
     var sonGoku = document.getElementById("Goku");
@@ -20,3 +32,4 @@ function page1() {
 }
 
 document.getElementById("startButton").addEventListener("click",startGame);
+    event.preventDefault();
