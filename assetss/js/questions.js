7549b764fb4ce4c35d367d1ddd3815a5d1948cc6
Brandon Perez <brandonp940@gmail.com>
2021-01-01 14:45:39 -0500

    added format for questions and answers

---
+++ assetss/js/questions.js
@@ -0,0 +1,63 @@
+// list of questions, choices, and answers
+
+var questions = [
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+
+    {
+        title:"",
+        choices:[],
+        ansewers:"",
+    },
+]
