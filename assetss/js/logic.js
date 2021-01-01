fea30ba22da62927f6484f7ca389df222f71038e
Brandon Perez <brandonp940@gmail.com>
2021-01-01 18:03:45 -0500

    added variables for quiz state

---
+++ assetss/js/logic.js
@@ -0,0 +1,7 @@
+//variables to track quiz state
+var currentQuestion = 0;
+var time = questions.length * 15;
+var timerId;
+
+// variables to reference Dom elements
+var questionsE1 = document.getElementById("questions");
